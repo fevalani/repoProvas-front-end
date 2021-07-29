fe565852b74d900c8db686ef6264642658c072ac
Fernando Valani Cavalcante <valanife@gmail.com>
2021-07-29 01:59:00 -0300

    create reset css and globalstyle

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import GlobalStyle from "../styles/GlobalStyle";
+import "../styles/reset.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+    </div>
+  );
+}
